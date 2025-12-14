64a3f9050a49f3d0b91b35936e81b154a5b741f7
Madhav Khating <mkhating9@gmail.com>
2025-12-14 10:16:24 +0530

    Second Commit- variable, output TF file added and VPC added

--- main.tf
+++ main.tf
@@ -2,6 +2,21 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames    = true
+}
+
+
 data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
@@ -13,9 +28,14 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
+
+  
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
+
 
   tags = {
-    Name = "learn-terraform"
+    Name = var.instance_name
   }
 }
